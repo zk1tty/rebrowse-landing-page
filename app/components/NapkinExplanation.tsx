445ce2e76e0388e5c110ddbf6ed39813d5bc5cc5
zk1tty <norikazkitty@gmail.com>
2025-05-03 18:15:44 +0800

    update bell-curve image to UVP

--- app/components/NapkinExplanation.tsx
+++ app/components/NapkinExplanation.tsx
@@ -8,27 +8,15 @@ const NapkinExplanation: React.FC = () => {
   return (
     <div className="mx-auto px-4">
       <h2 className="text-2xl mb-6 border-b border-stone-800 pb-2 text-stone-50 mt-8">
-        ## Problem v.s. Solution
+        ## Why you choose Rebrowse?
       </h2>
       
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:max-w-[702px]">
           <div className="w-full max-w-[702px] mx-auto">
             <Image
-              src={rebrowseProblem}
+              src="/images/bell-curve-automation.jpg"
               alt="rebrowse Problem"
-              width={702}
-              height={279}
-              className="w-full h-auto"
-              priority
-            />
-          </div>
-        </div>
-        <div className="mx-auto max-w-2xl lg:max-w-[702px] mt-6">
-          <div className="w-full max-w-[702px] mx-auto">
-            <Image
-              src={rebrowseVision}
-              alt="rebrowse Vision"
               width={702}
               height={279}
               className="w-full h-auto"
