e442e5c75d72a2a06e84460a8882058f062b0e0c
zk1tty <norikazkitty@gmail.com>
2025-06-18 21:02:40 +0800

    add bell-curve-nori png

--- app/components/NapkinExplanation.tsx
+++ app/components/NapkinExplanation.tsx
@@ -20,7 +20,7 @@ const NapkinExplanation: React.FC = () => {
         <div className="mx-auto max-w-2xl lg:max-w-[702px]">
           <div className="w-full max-w-[702px] mx-auto">
             <Image
-              src="/images/bell-curve-with-logos.png"
+              src="/images/bell-curve-nori.png"
               alt="rebrowse Problem"
               width={702}
               height={279}
